7157eb505f8b01d889e71cf9d694c583a052eb0a
KhunKrit46 <khunkrit46@gmail.com>
2025-04-01 00:35:41 -0400

    add 309-e11

---
+++ app/csc309-e11/page.tsx
@@ -0,0 +1,8 @@
+export default function CSC309E11() {
+  return (
+    <div id="auto-tester">
+      Hi. I am kasikpan, and I have learned how to develop a full-stack web app
+      and deploy it!
+    </div>
+  );
+}
---
+++ scripts.js
@@ -0,0 +1,12 @@
+function getBaseUrl() {
+  return "https://e11-krit-kasikpans-projects.vercel.app";
+}
+
+function getUTorId() {
+  return "kasikpan";
+}
+
+module.exports = {
+  getBaseUrl,
+  getUTorId,
+};
